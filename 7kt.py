b1b2c368a8cf3c6b22de6f09dbcb23df4c5dea69
Fabio <f.giraud@gmail.com>
2022-04-24 11:23:45 +0200

    Update 7kt.py

--- 7kt.py
+++ 7kt.py
@@ -15,8 +15,10 @@ instrument.serial.timeout  = 0.05          # seconds
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 instrument.clear_buffers_before_each_transaction = True
 
+if what == 'voltage.fase_average': 
+    print(instrument.read_long(0x0034,3,False)/100)
 
-if what == 'voltage.fase_l1': 
+elif what == 'voltage.fase_l1': 
     print(instrument.read_long(0x0002,3,False)/100)
 
 elif what == "voltage.fase_l2": 
@@ -25,6 +27,8 @@ elif what == "voltage.fase_l2":
 elif what == "voltage.fase_l3": 
     print(instrument.read_long(0x0006,3,False)/100)
 
+elif what == "voltage.average": 
+    print(instrument.read_long(0x0036,3,False)/100)
 
 elif what == "voltage.l1": 
     print(instrument.read_long(0x000E,3,False)/100)
@@ -47,6 +51,9 @@ elif what == "current.l2":
 elif what == "current.l3": 
     print(instrument.read_long(0x000C,3,False)/10000)
 
+elif what == "power.average": 
+    print(instrument.read_long(0x003A,3,True)/100)
+
 elif what == "power.l1": 
     print(instrument.read_long(0x0014,3,True)/100)
 
@@ -58,10 +65,12 @@ elif what == "power.l3":
 
 elif what == "": 
     #Voltage of fase L1-L2-L3
+    print("voltage.fase_average:",instrument.read_long(0x0034,3,False)/100)
     print("voltage.fase_l1:",instrument.read_long(0x0002,3,False)/100)
     print("voltage.fase_l2:",instrument.read_long(0x0004,3,False)/100)
     print("voltage.fase_l3:",instrument.read_long(0x0006,3,False)/100)
     #Voltage L1-L2-L3
+    print("voltage.average:",instrument.read_long(0x0036,3,False)/100)
     print("voltage.l1:",instrument.read_long(0x000E,3,False)/100)
     print("voltage.l2:",instrument.read_long(0x0010,3,False)/100)
     print("voltage.l3:",instrument.read_long(0x0012,3,False)/100)
@@ -71,9 +80,11 @@ elif what == "":
     print("current.l1:",instrument.read_long(0x0008,3,False)/10000)
     print("current.l2:",instrument.read_long(0x000A,3,False)/10000)
     print("current.l3:",instrument.read_long(0x000C,3,False)/10000)
-    #Energia Attiva L1-L2-L3
+    #Active Power L1-L2-L3
+    print("power:",instrument.read_long(0x003A,3,True)/100)
     print("power.l1:",instrument.read_long(0x0014,3,True)/100)
     print("power.l2:",instrument.read_long(0x0016,3,True)/100)
     print("power.l3:",instrument.read_long(0x0018,3,True)/100)
+
 else: 
     print("NaN")
